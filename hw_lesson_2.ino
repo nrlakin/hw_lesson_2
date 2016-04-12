cf635be7c612041f4ce0f5991b3d2b42b80bb001
Neil Lakin <neil.lakin@gmail.com>
2016-04-12 18:07:17 -0400

    add skeleton

---
+++ hw_lesson_2.ino
@@ -0,0 +1,11 @@
+#include <Wire.h>
+#include "rgb_led.h"
+#include "accel_sensor.h"
+  
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
